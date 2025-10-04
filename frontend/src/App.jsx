2200241f57adad55c81fe6945a2b97c002ac7518
Cr1z <cristianalexperezalfaro2@gmail.com>
2025-10-04 01:14:17 -0600

    Vista Principal vacia

---
+++ frontend/src/App.jsx
@@ -0,0 +1,18 @@
+function App() {
+  return (
+    <div style={{ position: "relative", width: "100%", height: "100vh" }}>
+      <div
+        style={{
+          position: "absolute",
+          top: "50%",
+          left: "50%",
+          transform: "translate(-50%, -50%)",
+        }}
+      >
+        <h1>HHello Nasa</h1>
+      </div>
+    </div>
+  );
+}
+
+
