5b6dfc8c60350e10002f3512cba3a84b5dae4d4e
Pedro-cloud145 <pppedromendezzz@gmail.com>
2025-10-04 14:22:50 -0600

    control de pantalla

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # Check-now
 NASA Space Apps Challenge Guatemala 2025 project, made by Space Sand
+set
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,18 +1,16 @@
-function App() {
-  return (
-    <div style={{ position: "relative", width: "100%", height: "100vh" }}>
-      <div
-        style={{
-          position: "absolute",
-          top: "50%",
-          left: "50%",
-          transform: "translate(-50%, -50%)",
-        }}
-      >
-        <h1>HHello Nasa</h1>
-      </div>
-    </div>
-  );
-}
+import {usestate } from "react";
 
+function app () { 
+  const [pestaña, setpestaña] = usestate("Inicio"); // pestaña activa 
+  const [areaseleccionada, setAreaseleccionada] = usestate(null); 
+
+  // datos del clima simulados por cada area
+
+  const datosclima ={
+    "Area1 - Norte": { temperatura: "22°C", humedad: "60%", viento: "15 km/h" },
+    "Area2 - Sur": { temperatura: "25°C", humedad: "55%", viento: "10 km/h" },
+    "Area3 - Este": { temperatura: "20°C", humedad: "70%", viento: "20 km/h" },
+    "Area4 - Oeste": { temperatura: "23°C", humedad: "65%", viento: "12 km/h" },
+  };
+}
 
