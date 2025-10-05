e5a2fcabeb39a925a9a5778784149159009c1e7b
Cr1z <cristianalexperezalfaro2@gmail.com>
2025-10-04 20:18:39 -0600

    Interfaz funcionable

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,18 +1,25 @@
+
+import React, { useState } from 'react'; 
+
 function App() {
+  const [contador, setContador] = useState(0); 
   return (
-    <div style={{ position: "relative", width: "100%", height: "100vh" }}>
-      <div
-        style={{
-          position: "absolute",
-          top: "50%",
-          left: "50%",
-          transform: "translate(-50%, -50%)",
-        }}
-      >
-        <h1>HHello Nasa</h1>
-      </div>
+    <div className="App">
+      {/* 3. Escribe tus elementos HTML y componentes de React aquí */}
+      <h1>HELLO NASA</h1>
+      <p>
+        Este es el contenido de mi archivo <strong>App.jsx</strong>.
+      </p>
+      
+      {/* 4. Puedes inyectar variables de JavaScript con llaves {} */}
+      <button onClick={() => setContador(contador + 1)}>
+        Clics: {contador}
+      </button>
+      
+      {/* 5. Aquí podrías renderizar otros componentes (como <MiHeader />) */}
     </div>
   );
 }
 
-
+// 6. Exporta el componente para que pueda ser usado en el archivo principal (main.jsx)
+export default App;
