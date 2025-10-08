b901afdd981f9c9834f6a1065192772a63e31423
Cr1z <cristianalexperezalfaro2@gmail.com>
2025-10-07 22:17:17 -0600

    PAGINA WEB

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
+  // **AÑADE ESTA LÍNEA:** // Esto le dice a Vite que la aplicación está alojada en la raíz del servidor.
+  base: '/',
+  
   plugins: [react()],
 })
